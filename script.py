ab297ff507d80c62084a2c787b34e1e8edb5efa1
andrewsoleco <andrew.soleco@gmail.com>
2020-12-17 19:24:55 +0900

    Put some code

---
+++ script.py
@@ -0,0 +1,19 @@
+import sys
+import requests
+from os import rename
+import math
+
+# print(sys.version)
+# print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "안녕 애들아! 반가와! 나는 {} 라고 해!".format(who_to_greet)
+    return greeting
+
+
+# r = requests.get("http://www.soleco.jp")
+# print(r.status_code)
+print("fdfdf")
+# iroom = greet("박 민수")
+# print(iroom)
